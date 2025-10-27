e6a386c9066bef7820e87983d222d60000570813
JoonasLuotonen <joonas.luotonen@icloud.com>
2025-10-27 15:00:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  async redirects() {
+    return [{ source: "/clarity-test", destination: "/clarity", permanent: true }];
+  },
 };
 module.exports = nextConfig;
